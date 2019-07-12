b5250cc199518d947d456a5cd1ede1ff07c7ed57
Alex <alexanderperepechaev@gmail.com>
2019-07-12 10:56:18 +0300

    Функция для табов

---
+++ script.js
@@ -0,0 +1,36 @@
+   
+function createTabs(tabHeader, tabHeaderElem, tabContentElem){
+
+    let info = document.querySelector(tabHeader),
+        tab = document.querySelectorAll(tabHeaderElem),
+        tabContent = document.querySelectorAll(tabContentElem);
+
+        function hideTabContent(a){
+            for(let i = a; i < tabContent.length; i++){
+                tabContent[i].classList.remove('show');
+                tabContent[i].classList.add('hide');
+            }
+        }
+
+        hideTabContent(1);
+
+        function showTabContent(a){
+            if(tabContent[a].classList.contains('hide')){
+                tabContent[a].classList.remove('hide');
+                tabContent[a].classList.add('show');
+            }
+        }
+
+        info.addEventListener('click', function(e){
+            let target = e.target;
+            if(target && target.classList.contains('info-header-tab')){
+                for(let i = 0; i < tab.length; i++){
+                    if(target == tab[i]){
+                        hideTabContent(0);
+                        showTabContent(i);
+                    }
+                }
+            }
+        });
+
+    }
